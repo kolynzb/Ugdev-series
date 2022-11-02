9e49fec9634d946e9b191d97a2465a82b5b41e42
kolynzb <collinsbenda360@gmail.com>
2022-11-03 00:55:48 +0300

    Switch Ontap to Arrow FUNTION

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -53,36 +53,28 @@ class _HomePageState extends State<HomePage> {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               GestureDetector(
-                onTap: () {
-                  setState(() => pageIndex = 0);
-                },
+                onTap: () => setState(() => pageIndex = 0),
                 child: const Icon(
                   Icons.home,
                   color: Colors.white,
                 ),
               ),
               GestureDetector(
-                onTap: () {
-                  setState(() => pageIndex = 1);
-                },
+                onTap: () => setState(() => pageIndex = 1),
                 child: const Icon(
                   Icons.account_tree,
                   color: Colors.white,
                 ),
               ),
               GestureDetector(
-                onTap: () {
-                  setState(() => pageIndex = 3);
-                },
+                onTap: () => setState(() => pageIndex = 3),
                 child: const Icon(
                   Icons.notifications,
                   color: Colors.white,
                 ),
               ),
               GestureDetector(
-                onTap: () {
-                  setState(() => pageIndex = 2);
-                },
+                onTap: () => setState(() => pageIndex = 2),
                 child: const Icon(
                   Icons.settings,
                   color: Colors.white,
