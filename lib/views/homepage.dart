70726519219537456e5b4aabeef02df7e5c64ff2
kolynzb <collinsbenda360@gmail.com>
2022-11-02 20:33:14 +0300

    Add Linear Gradient

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -32,17 +32,18 @@ class _HomePageState extends State<HomePage> {
         body: pages[pageIndex!],
         bottomNavigationBar: Container(
           padding: const EdgeInsets.all(10),
-          margin: EdgeInsets.only(bottom: height * .02),
+          margin: EdgeInsets.only(
+              bottom: height * .02, left: width * .02, right: width * .02),
           height: 70,
           decoration: BoxDecoration(
             gradient: const LinearGradient(
-              colors: [
-                const Color(0xFF3366FF),
-                const Color(0xFF00CCFF),
-                const Color(0xFF3366FF),
+              colors: <Color>[
+                Color.fromARGB(255, 0, 0, 0),
+                Color.fromARGB(255, 44, 44, 44),
+                Color.fromARGB(255, 0, 0, 0),
               ],
-              begin: const FractionalOffset(0.0, 0.0),
-              end: const FractionalOffset(1.0, 0.0),
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
               stops: [0.0, 1.0, 0.0],
               tileMode: TileMode.clamp,
             ),
